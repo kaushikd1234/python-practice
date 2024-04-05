0b0485b4de5f7418bad672961b412510ffac0b70
kaushikd1234 <123860112+kaushikd1234@users.noreply.github.com>
2024-04-05 15:01:10 +0530

    Update test.py

--- consume-sqs-related/test.py
+++ consume-sqs-related/test.py
@@ -1,1 +1,30 @@
+import boto3
+
+# Initialize SQS client
+sqs = boto3.client('sqs', region_name='us-east-1')  # Change the region as per your SQS queue's region
+
+def consume_from_sqs():
+    # Replace 'your_queue_url' with the URL of your SQS queue
+    queue_url = 'https://sqs.us-east-1.amazonaws.com/891377293198/test-cli-sqs'
+
+    # Retrieve messages from the SQS queue
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        MaxNumberOfMessages=10  # Adjust this value based on your requirements
+    )
+
+    # Process received messages
+    messages = response.get('Messages', [])
+    for message in messages:
+        # Print message body
+        print("Received message:", message['Body'])
+
+        # Delete the message from the queue
+        sqs.delete_message(
+            QueueUrl=queue_url,
+            ReceiptHandle=message['ReceiptHandle']
+        )
+
+if __name__ == "__main__":
+    consume_from_sqs()
 
